f98315e9450946fb5f9f809228503fd30b7834bb
arif <rainak_77@yahoo.com>
2024-01-30 10:15:39 +0200

    Created Home.jsx and Update.jsx file

--- reduxToolKit/src/main.jsx
+++ reduxToolKit/src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { Provider } from 'react-redux'
+import {configureStore} from '@reduxjs/toolkit'
+
+const store = configureStore({
+  reducer: {
+    // user:
+  }
+})
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode store={}>
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
